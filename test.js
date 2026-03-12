b983bf3ef883dcfe3dab95f850319b59a72f3b64
Arindol Ghosh <144908811+arindolghosh44@users.noreply.github.com>
2026-03-12 12:40:17 +0530

    var const let

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("hi")
+
+const accountId = 144553  // no changes
+
+let accountMail = "arindod@gmail.com" // block scope
+
+var accountpassword = "12344" // global scope
+
+accountCity = "asansol"
+
+let accountState
+
+/*
+best used let and const
+
+*/ 
+
+console.table([accountId,accountMail,accountpassword,accountCity,accountState])
